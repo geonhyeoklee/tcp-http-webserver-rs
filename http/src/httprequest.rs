8afb0a852ddb923deaf691362ebb7837b5307216
GeonHyeokLee <geonhyeoklee.kr@gmail.com>
2024-06-26 08:56:29 +0900

    feat: add httpserver

--- http/src/httprequest.rs
+++ http/src/httprequest.rs
@@ -108,31 +108,28 @@ impl From<&str> for Version {
     }
 }
 
-mod tests {
-    use super::*;
-    #[test]
-    fn test_method_into() {
-        let m: Method = "GET".into();
-        assert_eq!(m, Method::Get);
-    }
+#[test]
+fn test_method_into() {
+    let m: Method = "GET".into();
+    assert_eq!(m, Method::Get);
+}
 
-    #[test]
-    fn test_version_into() {
-        let m: Version = "HTTP/1.1".into();
-        assert_eq!(m, Version::V1_1)
-    }
+#[test]
+fn test_version_into() {
+    let m: Version = "HTTP/1.1".into();
+    assert_eq!(m, Version::V1_1)
+}
 
-    #[test]
-    fn test_read_http() {
-        let s = String::from("GET /greeting HTTP/1.1\r\nHost: localhost:3000\r\nUser-Agent: curl/7.64.1\r\nAccept: */*\r\n\r\n");
-        let mut headers_expected = HashMap::new();
-        headers_expected.insert("Host".into(), " localhost".into());
-        headers_expected.insert("Accept".into(), " */*".into());
-        headers_expected.insert("User-Agent".into(), " curl/7.64.1".into());
-        let req: HttpRequest = s.into();
-        assert_eq!(Method::Get, req.method);
-        assert_eq!(Version::V1_1, req.version);
-        assert_eq!(Resource::Path("/greeting".to_string()), req.resource);
-        assert_eq!(headers_expected, req.headers);
-    }
+#[test]
+fn test_read_http() {
+    let s = String::from("GET /greeting HTTP/1.1\r\nHost: localhost:3000\r\nUser-Agent: curl/7.64.1\r\nAccept: */*\r\n\r\n");
+    let mut headers_expected = HashMap::new();
+    headers_expected.insert("Host".into(), " localhost".into());
+    headers_expected.insert("Accept".into(), " */*".into());
+    headers_expected.insert("User-Agent".into(), " curl/7.64.1".into());
+    let req: HttpRequest = s.into();
+    assert_eq!(Method::Get, req.method);
+    assert_eq!(Version::V1_1, req.version);
+    assert_eq!(Resource::Path("/greeting".to_string()), req.resource);
+    assert_eq!(headers_expected, req.headers);
 }
--- http/src/httpresponse.rs
+++ http/src/httpresponse.rs
@@ -109,69 +109,66 @@ impl<'a> From<HttpResponse<'a>> for String {
     }
 }
 
-mod tests {
-    use super::*;
-    #[test]
-    fn test_response_struct_creation_200() {
-        let response_actual = HttpResponse::new(
-            "200",
-            None,
-            Some("Item was shipped on 21st Dec 2020".into()),
-        );
+#[test]
+fn test_response_struct_creation_200() {
+    let response_actual = HttpResponse::new(
+        "200",
+        None,
+        Some("Item was shipped on 21st Dec 2020".into()),
+    );
 
-        let response_expected = HttpResponse {
-            version: "HTTP/1.1",
-            status_code: "200",
-            status_text: "OK",
-            headers: {
-                let mut h = HashMap::new();
-                h.insert("Content-Type", "text/html");
-                Some(h)
-            },
-            body: Some("Item was shipped on 21st Dec 2020".into()),
-        };
+    let response_expected = HttpResponse {
+        version: "HTTP/1.1",
+        status_code: "200",
+        status_text: "OK",
+        headers: {
+            let mut h = HashMap::new();
+            h.insert("Content-Type", "text/html");
+            Some(h)
+        },
+        body: Some("Item was shipped on 21st Dec 2020".into()),
+    };
 
-        assert_eq!(response_actual, response_expected);
-    }
+    assert_eq!(response_actual, response_expected);
+}
 
-    #[test]
-    fn test_response_struct_creation_404() {
-        let response_actual = HttpResponse::new(
-            "404",
-            None,
-            Some("Item was shipped on 21st Dec 2020".into()),
-        );
+#[test]
+fn test_response_struct_creation_404() {
+    let response_actual = HttpResponse::new(
+        "404",
+        None,
+        Some("Item was shipped on 21st Dec 2020".into()),
+    );
 
-        let response_expected = HttpResponse {
-            version: "HTTP/1.1",
-            status_code: "404",
-            status_text: "Not Found",
-            headers: {
-                let mut h = HashMap::new();
-                h.insert("Content-Type", "text/html");
-                Some(h)
-            },
-            body: Some("Item was shipped on 21st Dec 2020".into()),
-        };
+    let response_expected = HttpResponse {
+        version: "HTTP/1.1",
+        status_code: "404",
+        status_text: "Not Found",
+        headers: {
+            let mut h = HashMap::new();
+            h.insert("Content-Type", "text/html");
+            Some(h)
+        },
+        body: Some("Item was shipped on 21st Dec 2020".into()),
+    };
 
-        assert_eq!(response_actual, response_expected);
-    }
+    assert_eq!(response_actual, response_expected);
+}
 
-    #[test]
-    fn test_http_response_creation() {
-        let response_expected = HttpResponse {
-            version: "HTTP/1.1",
-            status_code: "404",
-            status_text: "Not Found",
-            headers: {
-                let mut h = HashMap::new();
-                h.insert("Content-Type", "text/html");
-                Some(h)
-            },
-            body: Some("Item was shipped on 21st Dec 2020".into()),
-        };
-        let http_string: String = response_expected.into();
-        let response_actual = "HTTP/1.1 404 Not Found\r\nContent-Type:text/html\r\nContent-Length: 33\r\n\r\nItem was shipped on 21st Dec 2020";
-        assert_eq!(http_string, response_actual)
-    }
+#[test]
+fn test_http_response_creation() {
+    let response_expected = HttpResponse {
+        version: "HTTP/1.1",
+        status_code: "404",
+        status_text: "Not Found",
+        headers: {
+            let mut h = HashMap::new();
+            h.insert("Content-Type", "text/html");
+            Some(h)
+        },
+        body: Some("Item was shipped on 21st Dec 2020".into()),
+    };
+    let http_string: String = response_expected.into();
+    let response_actual = "HTTP/1.1 404 Not Found\r\nContent-Type:text/html\r\nContent-Length: 33\r\n\r\nItem was shipped on 21st Dec 2020";
+    assert_eq!(http_string, response_actual)
 }
